b8aabbd51e5d7bd57f525c3d56f3d0af9585efa0
Thales de Freitas Ferraz <Thales.Ferraz@rumolog.com>
2026-08-25 20:09:12 -0300

    Site estático

--- core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.cco_view, name='cco'),
-    path('api/bloco/<str:codigo>/', views.atualizar_bloco, name='atualizar_bloco'),
-    path('api/amv/<str:codigo>/', views.alternar_amv, name='alternar_amv'),
-    path('api/trem/<str:codigo>/', views.atualizar_trem, name='atualizar_trem'),
-]
